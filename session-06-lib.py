f51ecb38fb5e667202f10853da8835cea597cfe6
parsa999 <309002576+jtk999py@users.noreply.github.com>
2026-09-06 09:02:21 +0330

    Create session-06-lib.py

---
+++ session-06-lib.py
@@ -0,0 +1,110 @@
+'''
+SESSION 6 ___ UNIVERSITY
+
+_________________________________________________________
+
+pep 8 ----> behtare age zarf , moteghayer ya listemoon esmesh 2 tikas vasat oon dota ye under line(_) beazerim
+
+mainlist=[] ---> main_list[]
+_________________________________________________________
+
+
+*tvabe TUPLE*
+
+tuple ba () tarif mishe va 2 ta tabe mohem be esm haye count va index dare dar edeme be toxijateshoon mipardazam
+
+
+
+COUNT:
+    khoroji int dare _ tetad anasor dakhel tuple ro ke mad nazaremoone ro baram barmigardoone va mitoonim berizimesh too zarf---> x.count('onsor morede nazar')
+
+******
+    
+INDEX:
+    khoroji int dare _  karesh ine ke index reshte morede nazaremoon ro baramoon barmigardooneh ---> x.index('tabe morede nazar')
+
+******
+
+NOKTEH !!! : baraye tarif tuple tak onsori bayad kenar oon onsor comma (,) tarif konim vagarna tuple hesab nemishe mesal --> x = (y) alan in syntax ba inke () dare tuple hesab nemishe chon bayad kenar tuple tak onsori comma tarif koni yani intori --> x = (y,)
+
+******
+
+KOLTEH!!: tuple vigegy unpack kardan dare yani to ye syntax mitooni be chanta moteghayer onsor tarif koni mesal --->  a,b,c = (10,13,51)
+alan a be 10 nesbat dade shode va b be 13 va c be 51. be in kar migan unpack kardan ke tuple in vegegy ro dare.
+
+*****
+
+________________________________________________________
+
+TAVABE DICTIONARY
+
+6 ta az mohem tarin tavabe dictionary ke too daneshgah tadris shod ro ba tozihatesh minevisam.
+
+******
+
+get : in tabe khoroji optional dare yani bastegy dare be value ee ke barash tariff mikonim __ karesh chiye ? --> barash ye key tarif mikonim age key ro peyda kard value oon ro neshonemoon mide vali age peyda nakone eror none mide.
+eror ro metoonim khodemoon ham tarif konim ke masalan age key ro peyda nakardi felan eror ya matn ro benevis baray user :
+x.get(key,matn eror)
+
+******
+
+values : vooroodi nadare vali khoroji dare ye meghdar(value) barash tarif mikoni va key oon value ro barat barmigardoone.
+x.values(key) 
+
+******
+
+tabe keys :  kilid haro barmigardooneh
+
+*****
+
+tabe items : voroodi nadare vali khoroji dare _ karesh chiye ? --> dictionary haro be shekl zoj moratab do ta dota barmigardoone baram
+
+list = ('a':1,'b':3,'c':5)
+x.items --> [('a',1),('b',3),('c',5)]
+
+*****
+
+tabe pop : ye value migire va ham kilid o ham value ro az dictionary hazf mikoneh
+
+*****
+
+tabe pop item: hamishe akharin key va value ro az list hazf mikoneh
+
+________________________________________________________
+
+TAVABE SET : 
+    dar edame tavabe mohem data type set ro tozih midam.
+    
+
+tabe add : khoroji nadare va karesh ine ke ye eleman migireh o oon ro be tor random dakhel list set ezafe mikoneh.
+
+*****
+
+tabe update : khoroji nadare _ karesh ine ke be set morede nazaremoon chanta chiz ro ke tarif mekonim ro ezafe mikoneh.
+
+tabe remove : value morede nazar ro az set hazf mikoneh.
+
+
+tabe discard : ye value migire va oon ro az set hazf mikoneh va age oon meghdar vojood nadashteh bashe bedoon hich taghiri set ro barmigardooneh.
+
+TAVABE NEMOODAR VEN SET :
+    
+unuion : ejtema migire
+
+pipe (|) : eshterak
+
+intersection: eshterak
+
+& : eshterak
+
+(x-y) : tafazol
+
+_______________________________________________________
+
+
+
+
+
+
+
+'''
